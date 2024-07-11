9eb88445accb67eebbf963303b99068ea059cd8b
Anshika Malpani <138215307+Anshika-Malpani@users.noreply.github.com>
2024-07-11 21:38:26 +0530

    changes in home page heading

--- src/components/PageOne.jsx
+++ src/components/PageOne.jsx
@@ -36,12 +36,12 @@ const PageOne = () => {
           </div>
           <div className='absolute top-[1%] md:top-[15%] overflow-hidden z-20 md:z-0'>
             <div className='overflow-hidden'>
-              <h1 className='Consolas heading text-[15vw] md:text-[15vw] lg:text-[10vw] font-black text-[#385154] tracking-tight leading-none md:ml-2'>
+              <h1 className='Consolas heading text-[15vw] md:text-[15vw] lg:text-[10vw] font-[1000] text-[#385154] tracking-tight leading-none md:ml-2'>
                 SCATCH
               </h1>
             </div>
             <div className='overflow-hidden'>
-              <h1 className='Consolas heading text-[22vw] md:text-[30vw] lg:text-[20vw] font-black text-[#385154] leading-none tracking-tight'>
+              <h1 className='Consolas heading text-[20vw] md:text-[30vw] lg:text-[20vw] font-[700] text-[#385154] leading-none tracking-tight'>
                 BACKPACK
               </h1>
             </div>
