ae6d4698e0635c374d3fc3a5d81ce9838a25c04b
Anshika Malpani <138215307+Anshika-Malpani@users.noreply.github.com>
2024-07-13 20:50:30 +0530

    c-21

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ProductCard = ({ bagImage, title, amount }) => {
   return (
-    <div  className="card w-[90%] h-[58vh] md:w-[40vw] lg:w-[23vw]  md:h-[65vh]  border-[2.5px] border-[#9694949d] rounded-xl pb-4 md:pb-[1vw] bg-[#D3DEDC] flex-shrink-0 cursor-pointer transition-all duration-300 ease-in-out hover:bg-black hover:text-white hover:scale-105 hover:border-none">
+    <div  className="card w-[90%] h-[57vh] md:w-[40vw] lg:w-[23vw]  md:h-[65vh]  border-[2.5px] border-[#9694949d] rounded-xl pb-4 md:pb-[1vw] bg-[#D3DEDC] flex-shrink-0 cursor-pointer transition-all duration-300 ease-in-out hover:bg-black hover:text-white hover:scale-105 hover:border-none">
       <div className='w-full h-[32vh] md:h-[40vh]  flex items-center justify-center'>
         <div className='w-[90%] h-[90%] md:w-[80%] md:h-[80%] bg-slate-200 rounded-xl overflow-hidden'>
           <img className='w-full h-full object-cover' src={bagImage} alt="" />
@@ -10,8 +10,8 @@ const ProductCard = ({ bagImage, title, amount }) => {
       </div>
       <div className='w-full flex items-center justify-center mt-4 md:mt-0'>
         <div className='w-[90%] md:w-[80%] flex items-center justify-between '>
-          <h1 className='font-bold text-2xl md:text-[1.3vw]'>{title}</h1>
-          <h1 className='font-bold text-2xl md:text-[1.3vw]'>₹{amount}</h1>
+          <h1 className='font-bold text-[6vw] md:text-[1.3vw]'>{title}</h1>
+          <h1 className='font-bold text-[6vw] md:text-[1.3vw]'>₹{amount}</h1>
         </div>
       </div>
       <div className='w-full flex items-center justify-center mt-2 md:mt-[0.5vw]'>
