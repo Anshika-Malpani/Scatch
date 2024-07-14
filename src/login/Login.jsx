bed6e5107ab6291e13aefe934b3e9c00f9eddd2f
Anshika Malpani <138215307+Anshika-Malpani@users.noreply.github.com>
2024-07-14 14:35:01 +0530

    c-29

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
         gsap.to(".login", { display: "none", duration: 0 });
         if (isMobile) {
             gsap.to(".left", { y: "150%", duration: 0.2 });
-            gsap.to(".right", { y: "-70%", duration: 0.2 });
+            gsap.to(".right", { y: "-68%", duration: 0.2 });
             gsap.to(".loginLeft", { y: "122%", duration: 0.2 });
             gsap.to(".loginRight", { y: "-82%", duration: 0.2 });
         } else {
