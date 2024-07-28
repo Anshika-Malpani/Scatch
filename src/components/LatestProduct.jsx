194ba2a77a1c272140580c42ba54d73f89a446ed
Anshika Malpani <138215307+Anshika-Malpani@users.noreply.github.com>
2024-07-28 17:16:41 +0530

    c-34

--- src/components/LatestProduct.jsx
+++ src/components/LatestProduct.jsx
@@ -32,7 +32,7 @@ const LatestProduct = () => {
         <h1 ref={headingRef} className='text-[9vw]  md:text-5xl font-[1000]'>Discover Our Latest Products</h1>
       </div>
       <div className='w-full md:h-[80vh] flex flex-col gap-10 justify-center items-center relative flex-shrink-0 md:flex-row md:items-baseline '>
-        <ProductCard bagImage="bag11.png" title="Travelling Bagpack" amount="4999" />
+        <ProductCard bagImage="bag11.webp" title="Travelling Bagpack" amount="4999" />
         <ProductCard bagImage="bag2.avif" title="Office Bagpack" amount="1999" />
         <ProductCard bagImage="bag6.avif" title="Stylish Bagpack" amount="1499" />
         <NavLink to="/product" style={{ boxShadow: "0 5px 15px rgba(0, 0, 0, 0.35)" }}  className='absolute px-5 bg-[#385154]  text-white rounded-full py-3 text-sm md:bottom-6 left-[26%] -bottom-[5%] md:left-[44%] transition-all duration-300 ease-in-out hover:bg-[#000000da] hover:text-white hover:scale-105'> View All Our Products</NavLink>
